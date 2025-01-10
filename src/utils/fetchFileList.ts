10828c698dcf15ac778ed8e034232ac9873c30db
Ex-iT <vic.toothpick@gmail.com>
2025-01-10 11:03:43 +0100

    Add fetchFileList util

--- src/utils/fetchFileList.ts
+++ src/utils/fetchFileList.ts
@@ -1,11 +1,32 @@
 import { siteConfig } from '../config'
 
-// @TODO: handle errors
+export interface File {
+  name: string
+  path: string
+  sha: string
+  size: number
+  url: string
+  html_url: string
+  git_url: string
+  download_url: string
+  type: string
+  _links: {
+    self: string
+    git: string
+    html: string
+  }
+}
+
 export default function (tag: string, project: string) {
-  return fetch(`${siteConfig.filesContentUrl}/${tag}/${project}`, {
-    headers: {
-      Accept: 'application/vnd.github+json',
-      'X-GitHub-Api-Version': '2022-11-28',
-    },
-  })
+  const token = import.meta.env.GITHUB_AUTHORIZATION_BEARER_TOKEN
+  const headers: RequestInit['headers'] = {
+    Accept: 'application/vnd.github+json',
+    'X-GitHub-Api-Version': '2022-11-28',
+  }
+
+  if (token) {
+    headers['Authorization'] = `Bearer ${token}`
+  }
+
+  return fetch(`${siteConfig.filesContentUrl}/${tag}/${project}`, { headers })
 }
